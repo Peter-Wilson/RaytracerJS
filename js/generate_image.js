8426c23c60943935c41b792a1cf20625fab4edbc
Peter Wilson <pw12nb@brocku.ca>
2016-01-06 15:44:41 -0500

    included object detection and lighting

--- js/generate_image.js
+++ js/generate_image.js
@@ -1,42 +1,239 @@
-var h;
-var w;
+var height;
+var width;
 var r0;
+var objects;
+var defaultColor = [255,255,255];
+var pixelWidth;
+var pixelCenter;
+var light;
+var ambientI = 3;
+var lightI = 0.8;
+var specularF = 5;
+var maxRecursions = 2;
 
 //iterate through each pixel to get the raytraced colour
-function Start(width, height)
+function RayTrace()
 {
-	h = height;
-	w = width;
-	r0 = [width/2,height/2,-500];
+	r0 = [width/2,height/2,-500];	
 	
-	RayTrace(0,0);
+	for(var i = 0; i < height; i+=pixelWidth)
+	{
+		var row = "ROW:"+i/pixelWidth+",";
+		for(var j = 0; j < width; j+=pixelWidth)
+		{
+			row += GetColor(r0,calculateDirection(i+pixelCenter,j+pixelCenter), 0) + ",";
+		}
+		postMessage(row);
+	}
+	
+	postMessage("done");
 }
 
-function RayTrace(i,j)
-{
-	//raytrace for a pixel
-	var PixelColor = GetColor(i,j);
+function GetColor(vectorStart, vectorSlope, recursion)
+{	
+	var intersection = -1;
+	var point;
+	var normal;
+	var minDistance;
+	var obj;
+	var color = defaultColor; 
+	var reflection;
 	
-	//row completed
-	if(j > w){
-		j = 0;
-		i++;
-	}else
-		j++;
+	if(recursion >= maxRecursions) return color;
 	
-	//image processed
-	if(i > h) return;
+	for(var index = 0; index < objects.length; index++)
+	{
+		if(objects[index].type === "SPHERE")
+		{
+			var B = CalculateB(vectorStart, vectorSlope, objects[index].location);
+			var C = CalculateC(vectorStart, objects[index].location, objects[index].radius);
+			var temp = (Math.pow(B,2) - 4*C);
+			
+			if(temp >= 0)
+			{
+				var t = (-B - Math.sqrt(temp))/2;
+				if(t <= 0)	t = (-B + Math.sqrt(temp))/2;
+				if(t <= 0) return defaultColor;
+				
+				var distance = Distance(vectorStart, vectorSlope, t);
+				
+				if(!minDistance || distance < minDistance)
+				{
+					minDistance = distance;
+					obj = objects[index];
+					point = [(vectorStart[0] + vectorSlope[0]*t), (vectorStart[1] + vectorSlope[1]*t), (vectorStart[2] + vectorSlope[2]*t)];
+					normal = [(point[0]-objects[index].location[0])/objects[index].radius,
+								(point[1]-objects[index].location[1])/objects[index].radius,
+								(point[2]-objects[index].location[2])/objects[index].radius];
+					reflection = CalculateReflection(vectorSlope, point, normal);
+					color = calculateColor(objects[index], normal, point, reflection);
+				}				
+			}
+		}
+		else if(objects[index].type === "PLANE")
+		{
+			//check for plane intersection
+			var direction = objects[index].plane[0] * vectorSlope[0] +
+							objects[index].plane[1] * vectorSlope[1] +
+							objects[index].plane[2] * vectorSlope[2];
+							
+			if(direction < 0)
+			{
+				var d2 = objects[index].plane[0] * vectorStart[0] +
+							objects[index].plane[1] * vectorStart[1] +
+							objects[index].plane[2] * vectorStart[2];
+							
+				var t = -(d2+objects[index].plane[3])/direction;
+				if(t < 0) continue;
+				
+				var distance = Distance(vectorStart, vectorSlope, t);
+				
+				if(!minDistance || distance < minDistance)
+				{
+					minDistance = distance;
+					obj = objects[index];
+					point = [(vectorStart[0] + vectorSlope[0]*t), (vectorStart[1] + vectorSlope[1]*t), (vectorStart[2] + vectorSlope[2]*t)];
+					normal = [objects[index].plane[0],
+								objects[index].plane[1],
+								objects[index].plane[2]];
+								
+					reflection = CalculateReflection(vectorSlope, point, normal);	
+					color = calculateColor(objects[index], normal, point, reflection);
+				}	
+			}
+		}
+		
+		else if(objects[index].type === "POLYGON")
+		{
+			//calculate polygon intersection
+			var direction = objects[index].plane[0] * vectorSlope[0] +
+							objects[index].plane[1] * vectorSlope[1] +
+							objects[index].plane[2] * vectorSlope[2];
+							
+			if(direction < 0)
+			{
+				var d2 = objects[index].plane[0] * vectorStart[0] +
+							objects[index].plane[1] * vectorStart[1] +
+							objects[index].plane[2] * vectorStart[2];
+							
+				var t = -(d2+objects[index].plane[3])/direction;
+				if(t < 0) continue;
+				
+				var distance = Distance(vectorStart, vectorSlope, t);
+				var Hitpoint = [(vectorStart[0] + vectorSlope[0]*t), (vectorStart[1] + vectorSlope[1]*t), (vectorStart[2] + vectorSlope[2]*t)];
+				
+				if((!minDistance || distance < minDistance) && pointInside(objects[index].point1,objects[index].point2,objects[index].point3,Hitpoint))
+				{
+					minDistance = distance;
+					obj = objects[index];
+					point = [(vectorStart[0] + vectorSlope[0]*t), (vectorStart[1] + vectorSlope[1]*t), (vectorStart[2] + vectorSlope[2]*t)];
+					normal = [objects[index].plane[0],
+								objects[index].plane[1],
+								objects[index].plane[2]];
+					
+					reflection = CalculateReflection(vectorSlope, point, normal);			
+					color = calculateColor(objects[index], normal, point, reflection);
+				}	
+			}			
+		}
+	}	
 	
-	setTimeout("RayTrace("+i+","+j+")",5);
+	//if(obj && obj.reflective)
+	//	color += (GetColor(point, reflection, recursion+1));
+	
+	return color;
+}
+
+function calculateColor(object, normal, hitpoint, reflection)
+{		
+	var ambientVal = [	ambientI*object.ambient[0],
+					ambientI*object.ambient[1],
+					ambientI*object.ambient[2]];
+					
+	var v = [light.location[0]- hitpoint[0],
+						light.location[1]- hitpoint[1],
+						light.location[2]- hitpoint[2]];
+	
+	var cosA = 	(dot(v,normal))/(length(v)*length(normal));
+	
+	var temp = (ambientVal[0] + lightI*object.diffuse[0]*cosA);
+	
+	var cosB = (dot(reflection,v))/(length(reflection)*length(v));
+	
+	return  [ 	cap((object.color[0]*(ambientVal[0] + lightI*(object.diffuse[0]*cosA + object.specular[0]*Math.pow(cosB, specularF))))),
+				cap(object.color[1]*(ambientVal[1] + lightI*(object.diffuse[1]*cosA + object.specular[1]*Math.pow(cosB, specularF) ))),
+				cap(object.color[2]*(ambientVal[2] + lightI*(object.diffuse[2]*cosA + object.specular[2]*Math.pow(cosB, specularF) ))) ];
 	
 }
 
-function GetColor(i, j)
+function cap(number)
 {
-	var ray;
-	var rD = calculateDirection(i,j);
-	postMessage("output:"+"\nValue = "+rD[0]);
-	
+	number = Math.round(number);
+	if(number > 255) return 255;
+	if(number < 0) return 0;
+	return number;
+}
+
+function dot(a,b)
+{
+	return (a[0]*b[0] + a[1]*b[1] + a[2]*b[2]);	
+}
+
+function length(vector)
+{
+	return (Math.sqrt(Math.pow(vector[0],2) + Math.pow(vector[1],2) + Math.pow(vector[2],2)));
+}
+
+function strip(number) {
+    return (parseFloat(number).toPrecision(10));
+}
+
+function pointInside(p1,p2,p3,p)
+{
+	var b1, b2, b3;
+
+    b1 = sign(p, p1, p2) < 0;
+    b2 = sign(p, p2, p3) < 0;
+    b3 = sign(p, p3, p1) < 0;
+
+    return ((b1 == b2) && (b2 == b3));
+}
+
+function sign (p1, p2, p3)
+{
+    return (p1[0] - p3[0]) * (p2[1] - p3[1]) - (p2[0] - p3[0]) * (p1[1] - p3[1]);
+}
+
+//calculate the distance between 2 points
+function Distance(vectorStart, vectorSlope, t){
+	return (Math.pow((vectorStart[0] + vectorSlope[0]*t)-vectorStart[0],2)+
+			Math.pow((vectorStart[1] + vectorSlope[1]*t)-vectorStart[1],2)+
+			Math.pow((vectorStart[2] + vectorSlope[2]*t)-vectorStart[2],2));
+}
+
+//one of the equations used in the sphere intersection calculation
+function CalculateB(start, slope, sphere)
+{
+	return (2*(slope[0]*(start[0]-sphere[0])+ 
+				slope[1]*(start[1]-sphere[1])+ 
+				slope[2]*(start[2]-sphere[2])));
+}
+
+//return the direction vector of the reflection
+function CalculateReflection(vector, hitpoint, normal)
+{
+	var value = dot(vector, normal);
+	return [strip(vector[0] - 2*(value)*normal[0]),
+			strip(vector[1] - 2*(value)*normal[1]),
+			strip(vector[2] - 2*(value)*normal[2])];
+}
+
+function CalculateC(start, sphere, radius)
+{
+	return (Math.pow(start[0]-sphere[0],2)+
+			Math.pow(start[1]-sphere[1],2)+ 
+			Math.pow(start[2]-sphere[2],2)- 
+			Math.pow(radius,2));
 }
 
 //calculate the direction vector from the eye to the pixel
@@ -50,29 +247,17 @@ function calculateDirection(i,j)
 	return [direction[0]/magnitude, direction[1]/magnitude, direction[2]/magnitude];	
 }
 
-/*
-var RayTrace( rvec, rec_depth){ 
-	var color = '#';
-	if (rec_depth > MAX DEPTH) 
-		colour += "FFFFFF"; 
-	else { 
-	if RayIntersection (rvec, hitobj, hitpt, hitnorm) { 
-		local colour = shade(hitobj, hitpt, hitnorm) 
-		if has refl(hitobj) { 
-			refl vec = calc reflection(rvec, hitobj, hitpt, hitnorm); 
-			reflect colour = RayTrace((hitpt, refl vec), rec depth+1) 
-		} // if 
-		if has trans(hitobj) { 
-			trans vec = calc transmission(rvec, hitobj, hitpt, hitnorm) 
-			trans colour = RayTrace((hitpt, trans vec), rec depth+1) 
-		} // if 
-		colour = local colour + reflect colour + trans colour 
-	} // if 
-	else 
-		colour = background colour 
-	} // else 
-	return ( colour ) 
-} // program
-*/
 
-Start(640,480);
+onmessage = function(e)
+{
+	objects = JSON.parse(e.data[0]);
+	height = e.data[1];
+	width = e.data[2];
+	pixelWidth = e.data[3];
+	pixelCenter = pixelWidth/2;
+	postMessage("\nObjects:"+objects+"\nHeight:"+height+"\nWidth:"+width);
+	light = new Object();
+	light.location = [0,640,-100];
+	light.directionVector = [1/Math.sqrt(3),-1/Math.sqrt(3),3/Math.sqrt(3)];	
+	RayTrace();
+}
--- js/raytracer.js
+++ js/raytracer.js
@@ -2,12 +2,16 @@ var gl; // A global variable for the WebGL context
 var objects;
 var w;
 var output;
+var context;
+var pixelWidth = 4;
 
 function start() {
-  var canvas = document.getElementById("glcanvas");
+  var glcanvas = document.getElementById("glcanvas");
+  var canvas2d = document.getElementById("canvas2d");
+  context = canvas2d.getContext("2d");
 
   // Initialize the GL context
-  gl = initWebGL(canvas);
+  gl = initWebGL(glcanvas);
   
   // Only continue if WebGL is available and working
   
@@ -23,6 +27,18 @@ function start() {
   }
 }
 
+function Shape(type, location, radius, ambient, diffuse, specular, color, reflective, refractive) {
+	this.type = type;
+	this.location = location;
+	this.radius = radius;
+	this.diffuse = diffuse;
+	this.specular = specular;
+	this.ambient = ambient;
+	this.color = color;
+	this.reflective = reflective;
+	this.refractive = refractive;
+}
+
 function initWebGL(canvas) {
   gl = null;
   objects = [];
@@ -31,12 +47,40 @@ function initWebGL(canvas) {
     // Try to grab the standard context. If it fails, fallback to experimental.
     gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
 	//initialize the default objects
-	var sphere;
-	sphere.positionX = 0;
-	sphere.positionY = 0;
-	sphere.radius = 10;
+	var sphere = new Shape("SPHERE",[250,250,10],100,[0.2,0.2,0.2],[0.8,0.8,0.8],[1,1,1],[1,255,1],1,0);
+	var sphere2 = new Shape("SPHERE",[100,100,100],50,[0.2,0.2,0.2],[0.8,0.8,0.8],[1,1,1], [1,255,1],1,0);
+	var sphere3 = new Shape("SPHERE",[450,450,900],20,[0.2,0.2,0.2],[0.8,0.8,0.8],[1,1,1], [1,255,1],1,0);
+	var plane = new Shape;
+	plane.plane = [-1,0,0,455];
+	plane.diffuse = [0.8,0.8,0.8];
+	plane.specular = [1,1,1];
+	plane.ambient = [0.2,0.2,0.2];
+	plane.color = [255,1,1];
+	plane.reflective = 1;
+	plane.refractive = 0;
+	plane.type = "PLANE";
+	
+	
+	var triangle = new Shape;
+	triangle.point1 = [0,0,0];
+	triangle.point2 = [200,10,55];
+	triangle.point3 = [300,200,0];
+	triangle.type = "POLYGON";
+	triangle.diffuse = [0.8,0.8,0.8];
+	triangle.specular = [1,1,1];
+	triangle.ambient = [0.2,0.2,0.2];
+	triangle.color = [1,1,255];
+	triangle.reflective = 1;
+	triangle.refractive = 0;
+	triangle.plane = generatePlane(triangle.point1,triangle.point2,triangle.point3);
+	
+	
 	//add the default object to the list
 	objects.push(sphere);
+	objects.push(sphere2);
+	objects.push(sphere3);
+	objects.push(plane);
+	objects.push(triangle);
   }
   catch(e) {}
   
@@ -49,13 +93,39 @@ function initWebGL(canvas) {
   return gl;
 }
 
+//determine the plane of the polygon
+function generatePlane(point1, point2, point3)
+{
+	var vector1 = [point2[0]-point1[0],point2[1]-point1[1],point2[2]-point1[2]];
+	var vector2 = [point3[0]-point2[0],point3[1]-point2[1],point3[2]-point2[2]];
+	var crossproduct = [(vector1[1]*vector2[2] - vector1[2]*vector2[1]),
+						(vector1[2]*vector2[0] - vector1[0]*vector2[2]),
+						(vector1[0]*vector2[1] - vector1[1]*vector2[0])];
+	return [-crossproduct[0],-crossproduct[1],-crossproduct[2],
+		(crossproduct[0]*(-point1[0])+crossproduct[1]*(-point1[1])+crossproduct[2]*(-point1[2]))];
+}
+
 function startWorker() {
 	if(typeof(Worker) !== "undefined") {
 		if(typeof(w) == "undefined") {
 			w = new Worker("js/generate_image.js");
+			w.postMessage = w.webkitPostMessage || w.postMessage;
+			
+			w.postMessage([JSON.stringify(objects),480,640,pixelWidth]);
 		}
 		w.onmessage = function(event) {
-			output.innerHTML += event.data;
+			if(event.data.startsWith("ROW:")){
+				output.innerHTML += event.data;
+				var pixels = (event.data.substring(4)).split(',');
+				var x = pixels[0];
+				for(var j = 1; j < pixels.length-1; j+=3)
+				{
+					context.fillStyle = "rgb("+pixels[j]+","+pixels[j+1]+","+pixels[j+2]+")";
+					context.fillRect(((j-1)/3)*pixelWidth, x*pixelWidth, pixelWidth, pixelWidth );
+				}
+			}
+			else
+				output.innerHTML += event.data;
 		};
 	} else {
 		output.innerHTML = "Sorry, your browser does not support Web Workers...";
